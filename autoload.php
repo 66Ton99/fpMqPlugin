50f741e8f6f2afd7d6e83d6939a7a0f7014ff90f
Ton Sharp <66ton99@gmail.com>
2012-05-21 22:37:18 +0300

    Fixed bug

--- autoload.php
+++ autoload.php
@@ -7,7 +7,7 @@ require_once 'Zend/Queue/Message.php';
 require_once 'Zend/Queue/Adapter/AdapterAbstract.php';
 require_once 'Zend/Service/Amazon/Sqs.php';
 require_once 'Zend/Queue/Adapter/Memcacheq.php';
-require_once __DIR__ . '/lib/Zend/Queue/Adapter/AmazonSQS.class.php';
+require_once __DIR__ . '/lib/Zend/Queue/Adapter/AmazonSQS.php';
 require_once __DIR__ . '/lib/Zend/Queue/Adapter/Rabbitmq.php';
 require_once __DIR__ . '/lib/fpMqFunction.class.php';
 require_once __DIR__ . '/lib/fpMqDaemon.class.php';
